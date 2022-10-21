90de89265a74e6c9d2f814474b48f22733da98fa
Khushbu2314 <114127070+Khushbu2314@users.noreply.github.com>
2022-10-21 11:26:26 -0700

    Add files via upload

---
+++ 3.differential_analysis.r
@@ -0,0 +1,25 @@
+#Differential gene expression analysis in control and tumor genes
+setwd('C:\\Users\\sai\\Documents\\R')
+mat=matrix(NA,ncol=4,nrow = nrow(logcpm))
+rownames(mat)=rownames(logcpm)
+colnames(mat)=c('meanTumor','meanControl','pvalue','log2FC')
+
+for(i in 1:nrow(logcpm)){
+  vector1 = as.numeric(logcpm[i, 2:5])
+  
+  vector2 = as.numeric(logcpm[i, 6:9])
+  
+  res=t.test(vector1, vector2, paired = F, alternative = "two.sided")
+  mat[i,1]=res$estimate[[1]]
+  mat[i,2]=res$estimate[[2]]
+  mat[i,3]=res$p.value
+  mat[i,4]=mat[i,1]-mat[i,2]
+  
+}
+
+mat=as.data.frame(mat)
+num=which(is.nan(mat$pvalue))
+mat[num,'pvalue']=1
+
+library(EnhancedVolcano)
+EnhancedVolcano(mat,lab = rownames(mat),x = 'log2FC' ,y ='pvalue')
